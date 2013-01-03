3ae50ab6765cc9614d4519cdedcd83c7a81247af
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2013-01-03 21:15:51 +0100

    added spree_product_taxons fields to select list

--- app/controllers/spree/product_decorator.rb
+++ app/controllers/spree/product_decorator.rb
@@ -2,7 +2,7 @@
 module Spree
   Product.class_eval do
     add_search_scope :in_taxon do |taxon|
-      select("DISTINCT(spree_products.id), spree_products.*").
+      select("DISTINCT(spree_products.id), spree_products.*, spree_product_taxons.taxon_id, spree_product_taxons.position").
           joins(:taxons).
           where(Taxon.table_name => { :id => taxon.id })
     end
