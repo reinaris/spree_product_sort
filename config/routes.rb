d7eec5f998298fcbf097251a167077ded91d4918
Rein Aris <info@reinaris.nl>
2014-05-09 15:28:57 +0200

    Hardcoded link for now, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
-Spree::Core::Engine.routes.prepend do
-  
+Spree::Core::Engine.add_routes do
+
   namespace :admin do
 		# callback for jQuery sort action
-    match 'taxons/reorder_products/:id' => 'taxons#reorder_products', :via => :put
-    match 'product_taxons/positions' => 'product_taxons#positions', :as => :product_sort
+    get 'taxons/reorder_products/:id' => 'taxons#reorder_products', :as => :reorder_products, :via => :put
+    get 'product_taxons/positions' => 'product_taxons#positions', :as => :product_sort
     resources :product_taxons do
       collection do
         get :positions
       end
     end
-      
+
   end
 end
